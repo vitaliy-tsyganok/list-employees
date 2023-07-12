1501db96382faa008684455c5e286ff47ed885cf
Vitaliy Tsyganok <vitalytsiganok@gmail.com>
2023-07-12 16:09:56 +0300

    Верстка карточек + сайдбар + адаптив

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,25 @@ module.exports = {
   theme: {
     colors: {
       white: '#ffffff',
-      line: '#dbe4ed',
+      light: '#e8f1f4',
+      'light-2': '#e0ebef',
+      'light-3': '#dbe4ed',
+      'light-grey': '#e7f3ff',
       'dark-grey': '#84909b',
       grey: '#b0bcc7',
       black: '#041423',
       gold: '#e2bd06',
+      'light-gold': '#dcdcdc',
       red: '#e52e2e',
       'light-blue': '#00b6ed',
       green: '#00ae5b',
-      'light-grey': '#e7f3ff',
       blue: '#2a358c',
     },
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms'), require('prettier-plugin-tailwindcss')],
+  plugins: [
+    // require('prettier-plugin-tailwindcss'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
 };
