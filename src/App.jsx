eb0b7da5eba4ab725b254885021230a4e04d4c65
Vitaliy Tsyganok <vitalytsiganok@gmail.com>
2023-07-14 12:38:07 +0300

    Установил реакт + базовая очистка

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-indigo-500 text-center text-5xl font-semibold'>Hello World!</h1>
+    </>
+  )
+}
+
+export default App
