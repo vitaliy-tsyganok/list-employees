1cc31bff60c089f16176bc3f8e5b16535dc51807
Vitaliy Tsyganok <vitalytsiganok@gmail.com>
2023-07-20 12:14:49 +0300

    Отменил правило ESLint: react/prop-types

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,6 +16,8 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    
     'no-restricted-imports': ['error', { 'patterns': ['@*/*/*'] }],
+    'react/prop-types': ['none'],
   },
 };
